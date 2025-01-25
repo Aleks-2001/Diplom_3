63dcbd3e07ed880b47f8929e3a4155f3068946ba
Aleks-2001 <aleks.200175@gmail.com>
2025-01-25 20:34:45 +0300

    tests is ready

--- src/main/java/browser/Browser.java
+++ src/main/java/browser/Browser.java
@@ -10,23 +10,56 @@ public class Browser {
     public static void initDriver() throws IOException {
         Properties properties = new Properties();
         properties.load(new FileInputStream("src/test/resources/browser.properties"));
-        String browserProperty = properties.getProperty("testBrowser");
 
-        BrowserType browserType = BrowserType.valueOf (browserProperty);
+        String browserProperty = properties.getProperty("testBrowser"); // Получаем имя браузера
+        String remoteUrl = properties.getProperty("remoteUrl"); // Получаем URL для Selenium Grid
+
+        if (remoteUrl != null && !remoteUrl.isEmpty()) {
+            Configuration.remote = remoteUrl; // Устанавливаем RemoteWebDriver
+        }
+
+        BrowserType browserType = BrowserType.valueOf(browserProperty.toUpperCase());
         switch (browserType) {
             case CHROME:
-                Configuration.browser = "CHROME";
+                Configuration.browser = "chrome"; // Указываем браузер Chrome
                 break;
             case YANDEX:
-                Configuration.browser = "YANDEX";
+                Configuration.browser = "yandex";
                 break;
             case SAFARI:
-                Configuration.browser = "SAFARI";
+                Configuration.browser = "safari";
                 break;
             default:
                 throw new RuntimeException("Browser undefined");
         }
 
+        // Дополнительные настройки браузера
+        Configuration.browserCapabilities.setCapability("enableVNC", true); // Включить VNC (если используется Selenoid)
+        Configuration.browserCapabilities.setCapability("enableVideo", true); // Включить запись видео (если используется Selenoid)
     }
+
+
+
+//    public static void initDriver() throws IOException {
+//        Properties properties = new Properties();
+//        properties.load(new FileInputStream("src/test/resources/browser.properties"));
+//        String browserProperty = properties.getProperty("testBrowser");
+//
+//        BrowserType browserType = BrowserType.valueOf (browserProperty);
+//        switch (browserType) {
+//            case CHROME:
+//                Configuration.browser = "CHROME";
+//                break;
+//            case YANDEX:
+//                Configuration.browser = "YANDEX";
+//                break;
+//            case SAFARI:
+//                Configuration.browser = "SAFARI";
+//                break;
+//            default:
+//                throw new RuntimeException("Browser undefined");
+//        }
+//
+//    }
 
 }
