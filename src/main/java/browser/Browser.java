29a826de4ed51895220bcb94a89becfc5108716a
Aleks-2001 <aleks.200175@gmail.com>
2025-01-26 00:00:00 +0300

    tests is ready

--- src/main/java/browser/Browser.java
+++ src/main/java/browser/Browser.java
@@ -50,29 +50,4 @@ public class Browser {
 //        return options;
     }
 
-
-
-
-//    public static void initDriver() throws IOException {
-//        Properties properties = new Properties();
-//        properties.load(new FileInputStream("src/test/resources/browser.properties"));
-//        String browserProperty = properties.getProperty("testBrowser");
-//
-//        BrowserType browserType = BrowserType.valueOf (browserProperty);
-//        switch (browserType) {
-//            case CHROME:
-//                Configuration.browser = "CHROME";
-//                break;
-//            case YANDEX:
-//                Configuration.browser = "YANDEX";
-//                break;
-//            case SAFARI:
-//                Configuration.browser = "SAFARI";
-//                break;
-//            default:
-//                throw new RuntimeException("Browser undefined");
-//        }
-//
-//    }
-
 }
